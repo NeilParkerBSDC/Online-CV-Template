4c1886fc431ad9d2b53730a681138f4c7a61f35f
NeilParkerBSDC <60108423+NeilParkerBSDC@users.noreply.github.com>
2020-06-24 15:29:43 +0100

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "<nav>
+| <a href='index.php'>Home</a> | <a href='education.php'>Education</a> | <a href='wex.php'>Work Experience</a> | <a href='skills.php'>Skills</a> | <a href='portfolio.php'>Online Portfolio</a> | </nav>"
+
+?>
