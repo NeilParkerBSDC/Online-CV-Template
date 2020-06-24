827764d5eb40610f327ed04c3ff82bb1b651404e
NeilParkerBSDC <60108423+NeilParkerBSDC@users.noreply.github.com>
2020-06-24 15:27:31 +0100

    Create education.php

---
+++ education.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My CV</title>
+<link rel="stylesheet" type="text/css" href="MyStyle.css">
+</head>
+
+
+<body>
+
+
+<h1>Education</h1>
+<?php 
+include "navbar.php";
+//include connect.php;
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mycv";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM `education_tbl`";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  echo "<table  border=1><tr><th>Start</th><th>finish</th><th>Instituion</th><th>quals</th></tr>";
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<tr><td>".$row["StartDate"]."</td><td>".$row["EndDate"]."</td><td>".$row["Institution"]."</td><td>".$row["Qualifications"]."</td></tr>";
+  }
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+</body>
+</html>
