ddf243eca6f1ecf0a2319a40c372058d7461195d
NeilParkerBSDC <60108423+NeilParkerBSDC@users.noreply.github.com>
2020-06-24 15:25:34 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -5,40 +5,39 @@
     <link rel="stylesheet" type="text/css" href="MyStyle.css">
   </head>
   <body>
-  <h1>My CV</h1>
+  <h1>My CV </h1>
+  <!--
+ <nav>
+| <a href="index.php">Home</a> | <a href="education.php">Education</a> | <a href="wex.php">Work Experience</a> | <a href="skills.php">Skills</a> | <a href="portfolio.php">Online Portfolio</a> |
+ </nav>
+ -->
+ <?php
+include "navbar.php";
+ ?>
 
-
-<h2>My Code Dictionary</h2>
-<form action="GetMeCode.php" method="post">
+ <h2>Profile</h2>
+ <p>Some stuff about how wonderful I am and how you would really want to employ me because I am so briliant if you know what I mean ...</p>
+ <h2>About Me:</h2>
 <table border=0 cellpadding=1>
 <tr>
-<td>What do you want to do?</td>
-<td>In what Language?</td>
+<td class="BoldTD"> Name:</td>
+<td class="TDDetails"> Bob Robertson</td>
 </tr>
 <tr>
-<td>
-<select name="task"><!-- we can ulimately poplluate this from the database using PHP -->
-  <option value=""></option><!-- empty dummy option to stop  inadvertant selection of top item -->
-  <option value="Display">Display to Screen</option>
-  <option value="Prompt">Prompt for input</option>
-  <option value="Vardef">Define a variable</option>
-  <option value="Popup">Popup box</option>
-</select>
-</td>
-<td>
-<select name="language"><!-- we can ulimately poplluate this from the database using PHP -->
-  <option value=""></option><!-- empty dummy option to stop  inadvertant selection of top item -->
-  <option value="Python">Python</option>
-  <option value="Prompt">JavaScript</option>
-  <option value="Vardef">PHP</option>
-  <option value="Popup">C#</option>
-</select>
-</td>
+<td class="BoldTD"> Address:</td>
+<td class="TDDetails">16 Short Street<br/>Undertown<br/>Burton on Trent</br>Staffordshire<br/>D381TY</td>
 </tr>
-</table>
-<input type="submit">
-</form>
-
-
-</body>
+<tr>
+<td class="BoldTD"> Landline:</td>
+<td class="TDDetails"> 01235 123456</td>
+</tr>
+<tr>
+<td class="BoldTD"> Mobile:</td>
+<td class="TDDetails"> 0789 123456</td>
+</tr>
+<tr>
+<td class="BoldTD"> email:</td>
+<td class="TDDetails"> <a href="mailto:bob@mailme.com">bob@mailme.com</a></td>
+</tr>
+  </body>
 </html>
