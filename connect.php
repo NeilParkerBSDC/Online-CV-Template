0c82a63488385db6bdf3cfea1b6d7266429a88b2
NeilParkerBSDC <60108423+NeilParkerBSDC@users.noreply.github.com>
2022-03-21 18:58:58 +0000

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost"; // replace this with the actual servername for your database host
+$username = "root"; // replace this with the actual username for your database
+$password = ""; // replace this with the actual password for your database
+$dbname = "mycv"; // replace this with the actual name for your database
+?>
